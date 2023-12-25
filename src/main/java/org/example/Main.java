59f693073ff27f92b6f53eb6be7f477b6815e3f0
rishavtraining <65455921+rishavtraining@users.noreply.github.com>
2023-12-25 16:30:33 +0530

    Initial commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,11 @@
+package org.example;
+
+import java.util.Date;
+
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Hello world!");
+        System.out.println(new Date());
+    }
+}
